835774b2e6761eeffb0edbc005d8ff85e48fd432
ggnsall <ggnsall@naver.com>
2023-07-26 00:31:33 +0900

    [feat] 글로벌 스타일 설정

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+import '@emotion/react';
+declare module '@emotion/react' {
+  export interface Theme {
+    color: {
+      lightGray: string;
+      gray: string;
+    };
+    menuSize: {
+      width: number;
+      height: number;
+    };
+  }
+}
