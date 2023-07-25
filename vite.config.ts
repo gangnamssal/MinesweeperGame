ba7f1ee05f279dd376b91bef5219702539fe8172
ggnsall <ggnsall@naver.com>
2023-07-26 00:30:20 +0900

    [chore] 모듈 상대 경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: '@hooks', replacement: '/src/hooks' },
+      { find: '@helper', replacement: '/src/helper' },
+      { find: '@store', replacement: '/src/store' },
+      { find: '@style', replacement: '/src/style' },
+      { find: '@components', replacement: '/src/components' },
+    ],
+  },
+});
