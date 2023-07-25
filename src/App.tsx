de3499d5e16e626be35875fc7da1d342427e0e34
ggnsall <ggnsall@naver.com>
2023-07-26 00:35:22 +0900

    [feat] global 스타일 적용 및 지뢰찾기 게임 틀 완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,57 @@
+/** @jsxImportSource @emotion/react */
+import { css, Global, ThemeProvider } from '@emotion/react';
+
+import theme from '@style/theme';
+import Timer from '@components/Timer';
+import Minesweeper from '@components/Minesweeper';
+import DifficultySelect from '@components/DifficultySelect';
+
 function App() {
-  return <></>;
+  return (
+    <main>
+      <ThemeProvider theme={theme}>
+        <Global styles={globalCss} />
+        <section>
+          <article css={globalStyle.menus}>
+            {/* 난이도를 조정하는 select */}
+            <DifficultySelect />
+
+            {/* 타이머 */}
+            <Timer />
+          </article>
+
+          <article>
+            {/* 지뢰게임이 시작되는 컴포넌트 */}
+            <Minesweeper />
+          </article>
+        </section>
+      </ThemeProvider>
+    </main>
+  );
 }
+
+const globalCss = {
+  body: css({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '1280px',
+    height: '100vh',
+    margin: '0 auto',
+    padding: '0',
+    boxSizing: 'border-box',
+    backgroundColor: 'black',
+    color: 'white',
+  }),
+};
+
+const globalStyle = {
+  menus: () =>
+    css({
+      display: 'flex',
+      justifyContent: 'space-between',
+      marginBottom: '3%',
+    }),
+};
 
 export default App;
