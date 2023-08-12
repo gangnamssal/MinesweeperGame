df80302bc515e059b16840f2fc28ca943f242c3a
ggnsall <ggnsall@naver.com>
2023-08-12 22:33:07 +0900

    [feat] Minesweeper 컴포넌트 테스트코드 수정

--- src/components/Minesweeper/Minesweeper.test.tsx
+++ src/components/Minesweeper/Minesweeper.test.tsx
@@ -3,12 +3,13 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { Mock, describe, it, vi } from 'vitest';
 import userEvent from '@testing-library/user-event';
-import { render, renderHook, screen, waitFor } from '@testing-library/react';
+import { act, render, renderHook, screen, waitFor } from '@testing-library/react';
 
 import theme from '@style/theme';
 import mine from '@fixtures/mine';
 import useSetMine from '@hooks/useSetMine';
 import { setMinute } from '@store/timeSlice';
+import { changeMine } from '@store/mineSlice';
 import useCheckMine from '@hooks/useCheckMine';
 import Minesweeper from '@components/Minesweeper/Minesweeper';
 
@@ -39,33 +40,51 @@ describe('Minesweeper', () => {
   context('when I clicked on td tag', () => {
     it('is executed startGame function in isStart false', async () => {
       const cell_0_0 = screen.getByTestId('cell-0-0');
+
       const {
-        result: {
-          current: { setMine },
-        },
+        result: { current },
       } = renderHook(() => useSetMine());
+
+      const spy = vi.spyOn(current, 'setMine');
+
+      expect(spy).not.toBeCalled();
 
       await user.click(cell_0_0);
 
-      waitFor(() => {
-        expect(setMine).toBeCalled();
+      await waitFor(() => {
+        expect(dispatch).toBeCalledWith(changeMine(expect.any(Array)));
         expect(dispatch).toBeCalledWith(setMinute(5));
       });
     });
 
     it('is executed startGame function in isStart true', async () => {
       const cell_0_0 = screen.getByTestId('cell-0-0');
+
       const {
-        result: {
-          current: { checkMine },
-        },
+        result: { current },
       } = renderHook(() => useCheckMine());
+
+      const spy = vi.spyOn(current, 'checkMine');
+
+      expect(spy).not.toBeCalled();
 
       await user.click(cell_0_0);
 
-      waitFor(() => {
-        expect(checkMine).toBeCalled();
+      await waitFor(() => {
+        expect(dispatch).toBeCalledWith(changeMine(expect.any(Array)));
       });
     });
   });
+
+  // it('should change isStart when a td tag is clicked', async () => {
+  //   const cell_0_0 = screen.getByTestId('cell-0-0');
+
+  //   expect(mine.isStart).toBe(false);
+
+  //   await user.click(cell_0_0);
+
+  //   waitFor(() => {
+  //     expect(mine.isStart).toBe(true);
+  //   });
+  // });
 });
